86a3d8ae592c8c2b49388540d8f93fcd73bd094a
Machiastle <92347210+Machiastle@users.noreply.github.com>
2023-09-22 11:44:10 +0800

    Updated index, style, and js files
    Added new assets and started on skills feature

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
+//function for hamburger nav
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+//functions for skills button indicator
+// Get references to the buttons
+const techStackButton = document.getElementById('techStackButton');
+const toolsButton = document.getElementById('toolsButton');
+
+// Function to switch button styles
+function switchButtonStyles() {
+  // Toggle the "default_button" class for both buttons
+  techStackButton.classList.toggle('default_button');
+  toolsButton.classList.toggle('default_button');
+}
+
+// Add a click event listener to both buttons
+techStackButton.addEventListener('click', switchButtonStyles);
+toolsButton.addEventListener('click', switchButtonStyles);
